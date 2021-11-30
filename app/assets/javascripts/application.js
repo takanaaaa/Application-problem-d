61bb8433c1d07496784507ec50a6796206989411
EC2 Default User <ec2-user@ip-172-31-85-21.ec2.internal>
2021-11-30 12:40:51 +0000

    [FInish]7d評価

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,9 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery.raty.js
+
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
+
+//= require_tree
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_062142) do
+ActiveRecord::Schema.define(version: 2021_11_30_093200) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_08_30_062142) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "users", force: :cascade do |t|
